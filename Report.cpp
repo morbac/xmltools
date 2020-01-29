412256f55d109ac3d52b3bb0f5e80c7e73b0876a
Christian Grasser <christian.grasser@live.de>
2020-01-29 20:34:32 +0100

    - adapted appveyor.yml for usage with msxml - removed outdated, old visual studio files - fix compiler warning:    C:\projects\xmltools\Report.cpp(723,11): warning C4806: '&': unsafe operation: no value of type 'bool' promoted to type 'int' can equal the given constant [C:\projects\xmltools\XMLTools.vcxproj]

--- Report.cpp
+++ Report.cpp
@@ -720,7 +720,7 @@ bool Report::utf8CharToUcs2Char(const char* utf8Tok, wchar_t* ucs2Char, uint32_t
     *utf8TokLen = 4;
     return false;                        //Character exceeds the UCS-2 range (UCS-4 would be necessary)
   }
-  else if ((0xF8 == utf8TokUs[0] & 0xFC))
+  else if (0xF8 == (utf8TokUs[0] & 0xFC))
   {
     //Tokensize: 5 bytes
     *utf8TokLen = 5;
