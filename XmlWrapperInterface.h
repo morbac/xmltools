08fdca6eb33133d134e7771387637c0f177dd950
nc <morbac@gmail.com>
2020-10-04 12:11:05 +0200

    Add pure abstrct methods to xml wrapper interface

--- XmlWrapperInterface.h
+++ XmlWrapperInterface.h
@@ -1,6 +1,22 @@
 #pragma once
 
+#include <sstream>
+#include <vector>
+
 // WORK IN PROGRESS
+
+struct XPathResultEntryType {
+    std::string type;
+    std::string name;
+    std::string value;
+};
+
+struct ErrorEntryType {
+    size_t line;        // 0-based line number having the error
+    size_t linepos;     // 0-based position in line
+    size_t filepos;     // 0-based position in whole stream
+    std::string reason; // error description
+};
 
 /*
 * This abstract class is the interface for external XML-API wrappers.
@@ -13,16 +29,9 @@ public:
     XmlWrapperInterface() {}
     virtual ~XmlWrapperInterface() {}
 
-    // expected functions might be:
-    // - check xml syntax
-    // - check xml validity
-    // - current xml path --> might be done with simplexml or quickxml
-    // - xpath evaluation
-    // - xsl transform
-
-    // get error detail
-    // 
-    virtual void method1() = 0;    // "= 0" part makes this method pure virtual, and
-                                   // also makes this class abstract.
-    virtual void method2() = 0;
+    virtual bool checkSyntax(const char* xml, size_t size) = 0;
+    virtual bool checkValidity(const char* xml, size_t size) = 0;
+    virtual std::vector<XPathResultEntryType> xpathEvaluate(const char* xml, size_t size, std::wstring xpath, std::wstring ns) = 0;
+    virtual std::stringstream* xslTransform(const char* xml, size_t xmllen, const char* xsl, size_t xsllen) = 0;
+    virtual std::vector<ErrorEntryType> getLastErrors() = 0;
 };
